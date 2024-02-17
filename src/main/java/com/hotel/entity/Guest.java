406616621df14a8a5f8e9e44c7954e9f7d60ef98
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-02-17 12:08:05 +0100

    Initialization

--- src/main/java/com/hotel/entity/Guest.java
+++ src/main/java/com/hotel/entity/Guest.java
@@ -1,8 +1,88 @@
 package com.hotel.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
+@Table(name = "guests")
 public class Guest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guest_id")
+    private long id;
+    @Column(name = "first_name") // dobra praktyka dla SQL
+    private String firstName; // dobra praktyka dla JAVY
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email_address")
+    private String emailAddress;
+    private String address;
+    private String country;
+    private String state;
+    @Column(name = "phone_number")
+    private String phoneNumber;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
