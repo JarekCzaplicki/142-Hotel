49adf02c3686156f1853b20a1ebd1b2f4be52370
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-02-18 11:49:55 +0100

    Kolejne metody dla kontrolera

--- src/main/java/com/hotel/service/ReservationService.java
+++ src/main/java/com/hotel/service/ReservationService.java
@@ -76,18 +76,29 @@ public class ReservationService {
         return rooms;
     }
 
+    /**
+     * Method that is responsible for adding guests
+     * Guest cannot be null and cannot add existing guest
+     *
+     * @param guest Guest to add
+     * @return added guest from database
+     */
     // dodawaj gości hotelowych
     public Guest addGuest(Guest guest) {
         if (guest == null)
             throw new RuntimeException("Guest cannot be null");
+        Guest guest1 = guestRepository.findById(guest.getId()).orElse(null);
+
+        if (guest1 != null)
+            throw new RuntimeException("Guest already exist");
+
         return guestRepository.save(guest);
     }
 
     // usuwanie gości hotelowych
-    public void deleteGuest(long guestId) {
-        if (getHotelGuests().stream().noneMatch(guest -> guest.getId() == guestId))
-            throw new RuntimeException("Guest not found");
-
+    public Boolean deleteGuest(long guestId) {
+        guestRepository.findById(guestId).orElseThrow(() -> new RuntimeException("Guest by id not found"));
         guestRepository.deleteById(guestId);
+        return guestRepository.findById(guestId).isEmpty();
     }
 }
