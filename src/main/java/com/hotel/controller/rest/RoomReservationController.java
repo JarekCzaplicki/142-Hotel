e5816b3b4c0212d1c1933e6c9ecfc85cb4fb3aeb
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-02-18 12:35:00 +0100

    Koniec metod kontrollera Rest

--- src/main/java/com/hotel/controller/rest/RoomReservationController.java
+++ src/main/java/com/hotel/controller/rest/RoomReservationController.java
@@ -1,35 +1,56 @@
 package com.hotel.controller.rest;
 
 import com.hotel.entity.Guest;
+import com.hotel.entity.Room;
+import com.hotel.model.RoomReservation;
 import com.hotel.service.ReservationService;
+import com.hotel.util.DateUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/reservation")
 public class RoomReservationController {
     private final ReservationService reservationService;
+    private final DateUtil dateUtil;
 
-    public RoomReservationController(ReservationService reservationService) {
+    public RoomReservationController(ReservationService reservationService, DateUtil dateUtil) {
         this.reservationService = reservationService;
+        this.dateUtil = dateUtil;
     }
 
     @GetMapping("/guests")
     public List<Guest> getGuests() {
         return reservationService.getHotelGuests();
     }
+
     @PostMapping("/guests")
     @ResponseStatus(HttpStatus.CREATED)
-    public Guest addGuest(@RequestBody Guest guest){
+    public Guest addGuest(@RequestBody Guest guest) {
         return reservationService.addGuest(guest);
     }
+
     @DeleteMapping("/guests/{guestId}")
-    public boolean deleteGuest(@PathVariable long guestId){
+    public boolean deleteGuest(@PathVariable long guestId) {
         return reservationService.deleteGuest(guestId);
     }
 
+    @RequestMapping(method = RequestMethod.GET, path = "/rooms")
+    public List<Room> getRooms() {
+        return reservationService.getRooms();
+    }
 
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<RoomReservation> getReservations(@RequestParam(required = false, value = "date") String stringDate) {
+        Date date = dateUtil.createDateFromString(stringDate);
+        System.out.println("_______________________________");
+        System.out.println(stringDate);
+        System.out.println(date);
+        System.out.println("_______________________________");
+        return reservationService.getRoomsReservationsForDate(date);
+    }
 
 }
